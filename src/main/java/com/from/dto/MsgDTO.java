77c956c75c4969d57b81085a5703c9db16038291
최준영 <yj766352@gmail.com>
2026-08-26 15:16:27 +0900

    메시지 DTO 추가설명 작성

--- src/main/java/com/from/dto/MsgDTO.java
+++ src/main/java/com/from/dto/MsgDTO.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 
 @Builder
 public record MsgDto(
+        //오류 메시지 결과 담는 DTO
         //결과메시지 전달용,컨트롤러 -> 프론트로 결과 상태와 메시지를 전달하는 공통 DTO
         int result, //성공/실패 코드
         String msg, //사용자에게 보여줄 메시지
